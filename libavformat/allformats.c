85e261eaed670a289e9c8f8104438aebe1849d82
he.who.shall.not.be.named <notreply@example.com>
2018-04-02 22:13:58 -0700

    lavf: add vapoursynth demuxer
    modified by qyot27 to sit alongside the existing VapourSynth
    demuxer

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -529,6 +529,7 @@ extern const AVInputFormat  ff_libgme_demuxer;
 extern const AVInputFormat  ff_libmodplug_demuxer;
 extern const AVInputFormat  ff_libopenmpt_demuxer;
 extern const AVInputFormat  ff_vapoursynth_demuxer;
+extern const AVInputFormat  ff_vapoursynth_alt_demuxer;
 
 #include "libavformat/muxer_list.c"
 #include "libavformat/demuxer_list.c"
