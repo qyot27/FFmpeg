6d8729fc00d413a132dd6fd9c8c47c00757c7dbb
Stephen Hutchinson <qyot27@gmail.com>
2022-08-14 03:06:19 -0400

    swscale/avcodec: add YUVA422P14

--- libswscale/input.c
+++ libswscale/input.c
@@ -1658,6 +1658,7 @@ av_cold void ff_sws_init_input_funcs(SwsContext *c,
     case AV_PIX_FMT_YUVA422P12LE:
     case AV_PIX_FMT_YUVA444P12LE:
     case AV_PIX_FMT_YUVA420P14LE:
+    case AV_PIX_FMT_YUVA422P14LE:
     case AV_PIX_FMT_YUVA420P16LE:
     case AV_PIX_FMT_YUVA422P16LE:
     case AV_PIX_FMT_YUVA444P16LE:
@@ -1692,6 +1693,7 @@ av_cold void ff_sws_init_input_funcs(SwsContext *c,
     case AV_PIX_FMT_YUVA422P12BE:
     case AV_PIX_FMT_YUVA444P12BE:
     case AV_PIX_FMT_YUVA420P14BE:
+    case AV_PIX_FMT_YUVA422P14BE:
     case AV_PIX_FMT_YUVA420P16BE:
     case AV_PIX_FMT_YUVA422P16BE:
     case AV_PIX_FMT_YUVA444P16BE:
@@ -2080,6 +2082,7 @@ av_cold void ff_sws_init_input_funcs(SwsContext *c,
     case AV_PIX_FMT_YUVA422P12LE:
     case AV_PIX_FMT_YUVA444P12LE:
     case AV_PIX_FMT_YUVA420P14LE:
+    case AV_PIX_FMT_YUVA422P14LE:
     case AV_PIX_FMT_YUVA420P16LE:
     case AV_PIX_FMT_YUVA422P16LE:
     case AV_PIX_FMT_YUVA444P16LE:
@@ -2126,6 +2129,7 @@ av_cold void ff_sws_init_input_funcs(SwsContext *c,
     case AV_PIX_FMT_YUVA422P12BE:
     case AV_PIX_FMT_YUVA444P12BE:
     case AV_PIX_FMT_YUVA420P14BE:
+    case AV_PIX_FMT_YUVA422P14BE:
     case AV_PIX_FMT_YUVA420P16BE:
     case AV_PIX_FMT_YUVA422P16BE:
     case AV_PIX_FMT_YUVA444P16BE:
