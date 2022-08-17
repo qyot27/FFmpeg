f3c859186d63fd2c7b55764b05c945f6ddb1b460
Stephen Hutchinson <qyot27@gmail.com>
2022-08-17 18:37:59 -0400

    swscale: add YUVA420PF32

--- libswscale/format.c
+++ libswscale/format.c
@@ -272,6 +272,8 @@ static const FormatEntry format_entries[] = {
     [AV_PIX_FMT_YUV422PF32LE]   = { 1, 1 },
     [AV_PIX_FMT_YUV444PF32BE]   = { 1, 1 },
     [AV_PIX_FMT_YUV444PF32LE]   = { 1, 1 },
+    [AV_PIX_FMT_YUVA420PF32BE]  = { 1, 1 },
+    [AV_PIX_FMT_YUVA420PF32LE]  = { 1, 1 },
 };
 
 int sws_isSupportedInput(enum AVPixelFormat pix_fmt)
