6575a3f7c65822ce9f3e58de1481cd59b24b52d0
Stephen Hutchinson <qyot27@gmail.com>
2022-08-19 01:47:51 -0400

    nut/raw: add yuva420pf32

--- libavformat/nut.c
+++ libavformat/nut.c
@@ -216,6 +216,9 @@ const AVCodecTag ff_nut_video_tags[] = {
     { AV_CODEC_ID_RAWVIDEO,         MKTAG('Y', '3',  0 , 33 ) },
     { AV_CODEC_ID_RAWVIDEO,         MKTAG(33 ,  0 , '3', 'Y') },
 
+    { AV_CODEC_ID_RAWVIDEO,         MKTAG('Y', '4', 11 , 33 ) },
+    { AV_CODEC_ID_RAWVIDEO,         MKTAG(33 , 11 , '4', 'Y') },
+
     { AV_CODEC_ID_RAWVIDEO,         MKTAG('X', 'Y', 'Z' , 36 ) },
     { AV_CODEC_ID_RAWVIDEO,         MKTAG(36 , 'Z' , 'Y', 'X') },
 
