853a8f897157e35cb0051b22c20995fe6a57e10c
Stephen Hutchinson <qyot27@gmail.com>
2022-08-18 22:36:34 -0400

    nut/raw: add grayf32

--- libavformat/nut.c
+++ libavformat/nut.c
@@ -206,6 +206,9 @@ const AVCodecTag ff_nut_video_tags[] = {
     { AV_CODEC_ID_RAWVIDEO,         MKTAG('G', '4',   0,  33) },
     { AV_CODEC_ID_RAWVIDEO,         MKTAG(33 ,  0 , '4', 'G') },
 
+    { AV_CODEC_ID_RAWVIDEO,         MKTAG('Y', '1',   0, 33 ) },
+    { AV_CODEC_ID_RAWVIDEO,         MKTAG(33 ,   0, '1', 'Y') },
+
     { AV_CODEC_ID_RAWVIDEO,         MKTAG('X', 'Y', 'Z' , 36 ) },
     { AV_CODEC_ID_RAWVIDEO,         MKTAG(36 , 'Z' , 'Y', 'X') },
 
