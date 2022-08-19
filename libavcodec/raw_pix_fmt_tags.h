4c9a12a3fd9cd764df7d74e79a393176109b119f
Stephen Hutchinson <qyot27@gmail.com>
2022-08-19 02:09:53 -0400

    nut/raw: add yuva444pf32

--- libavcodec/raw_pix_fmt_tags.h
+++ libavcodec/raw_pix_fmt_tags.h
@@ -242,6 +242,8 @@ static const PixelFormatTag raw_pix_fmt_tags[] = {
     { AV_PIX_FMT_YUVA420PF32BE,    MKTAG(33 , 11 , '4', 'Y') },
     { AV_PIX_FMT_YUVA422PF32LE,    MKTAG('Y', '4', 10 , 33 ) },
     { AV_PIX_FMT_YUVA422PF32BE,    MKTAG(33 , 10 , '4', 'Y') },
+    { AV_PIX_FMT_YUVA444PF32LE,    MKTAG('Y', '4',  0 , 33 ) },
+    { AV_PIX_FMT_YUVA444PF32BE,    MKTAG(33 ,  0 , '4', 'Y') },
 
     { AV_PIX_FMT_XYZ12LE,         MKTAG('X', 'Y', 'Z' , 36 ) },
     { AV_PIX_FMT_XYZ12BE,         MKTAG(36 , 'Z' , 'Y', 'X') },
